9a2a8fcafb92fe1d7e7e42703255d3f11777fc42
Jonathan Roy <jonroy7@gmail.com>
2014-02-12 16:07:06 +0000

    set share diff target based on address hash rate

--- p2pool/work.py
+++ p2pool/work.py
@@ -216,7 +216,7 @@ class WorkerBridge(worker_interface.WorkerBridge):
     def _estimate_local_hash_rate(self):
         if len(self.recent_shares_ts_work) == 50:
             hash_rate = sum(work for ts, work in self.recent_shares_ts_work[1:])//(self.recent_shares_ts_work[-1][0] - self.recent_shares_ts_work[0][0])
-            if hash_rate > 0:
+            if hash_rate:
                 return hash_rate
         return None
     
@@ -291,8 +291,9 @@ class WorkerBridge(worker_interface.WorkerBridge):
         
         if desired_share_target is None:
             desired_share_target = 2**256-1
-            local_hash_rate = self._estimate_local_hash_rate()
-            if local_hash_rate is not None:
+            local_addr_rates = self.get_local_addr_rates()
+            local_hash_rate = local_addr_rates.get(pubkey_hash, 0)
+            if local_hash_rate > 0.0:
                 desired_share_target = min(desired_share_target,
                     bitcoin_data.average_attempts_to_target(local_hash_rate * self.node.net.SHARE_PERIOD / 0.0167)) # limit to 1.67% of pool shares by modulating share difficulty
             
@@ -371,22 +372,14 @@ class WorkerBridge(worker_interface.WorkerBridge):
         lp_count = self.new_work_event.times
         merkle_link = bitcoin_data.calculate_merkle_link([None] + other_transaction_hashes, 0) if share_info.get('segwit_data', None) is None else share_info['segwit_data']['txid_merkle_link']
         
-        if print_throttle is 0.0:
-            print_throttle = time.time()
-        else:
-            current_time = time.time()
-            if (current_time - print_throttle) > 5.0:
-                print 'New work! Diff: %.02f Share diff: %.02f Block value: %.2f %s (%i tx, %.0f kB)' % (
-                    bitcoin_data.target_to_difficulty(target),
-                    bitcoin_data.target_to_difficulty(share_info['bits'].target),
-                    self.current_work.value['subsidy']*1e-8, self.node.net.PARENT.SYMBOL,
-                    len(self.current_work.value['transactions']),
-                    sum(map(bitcoin_data.tx_type.packed_size, self.current_work.value['transactions']))/1000.,
-                )
-                print_throttle = time.time()
-
-        #need this for stats
-        self.last_work_shares.value[bitcoin_data.pubkey_hash_to_address(pubkey_hash, self.node.net.PARENT)]=share_info['bits']
+        print 'New work for worker %s! Difficulty: %.06f Share difficulty: %.06f (speed %.06f) Total block value: %.6f %s including %i transactions' % (
+            pubkey_hash,
+            bitcoin_data.target_to_difficulty(target),
+            bitcoin_data.target_to_difficulty(share_info['bits'].target),
+            local_addr_rates.get(pubkey_hash, 0),
+            self.current_work.value['subsidy']*1e-8, self.node.net.PARENT.SYMBOL,
+            len(self.current_work.value['transactions']),
+        )
         
         ba = dict(
             version=max(self.current_work.value['version'], 0x20000000),
